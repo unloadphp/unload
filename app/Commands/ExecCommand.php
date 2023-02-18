c66cf22629f160849f5dc7aa42743d17572747cb
yhortuk <17699384+yhortuk@users.noreply.github.com>
2023-02-18 21:27:29 +0200

    fix: trim php artisan from exec command request

--- app/Commands/ExecCommand.php
+++ app/Commands/ExecCommand.php
@@ -11,7 +11,7 @@ class ExecCommand extends Command
 
     public function handle(Lambda $lambda): void
     {
-        $command = implode(' ', $this->argument('arguments'));
+        $command = str_replace(['php artisan ', 'artisan '], '', implode(' ', $this->argument('arguments')));
 
         $this->newLine();
         $this->info("Running '{$command}' in {$this->unload->cliFunction()} function");
