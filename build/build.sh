17282b773d35e792b3001c8b97a8562695c60376
yhortuk <17699384+yhortuk@users.noreply.github.com>
2023-08-11 18:54:54 +0300

    feat: release pipeline

--- build/build.sh
+++ build/build.sh
@@ -7,10 +7,7 @@ $SCRIPT_DIR/../unload app:build --build-version=$LATEST_TAG
 
 echo "Creating static binaries..."
 
-mkdir -p $SCRIPT_DIR/../builds/linux
-mkdir -p $SCRIPT_DIR/../builds/macos
-
-cat $SCRIPT_DIR/linux.sfx $SCRIPT_DIR/../builds/unload > $SCRIPT_DIR/../builds/linux/unload
-cat $SCRIPT_DIR/macos.sfx $SCRIPT_DIR/../builds/unload > $SCRIPT_DIR/../builds/macos/unload
+cat $SCRIPT_DIR/linux.sfx $SCRIPT_DIR/../builds/unload > $SCRIPT_DIR/../builds/unload-$LATEST_TAG-linux
+cat $SCRIPT_DIR/macos.sfx $SCRIPT_DIR/../builds/unload > $SCRIPT_DIR/../builds/unload-$LATEST_TAG-macos
 
 echo "Build complete!"
